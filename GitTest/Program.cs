f83af4e382c8bfa38fb1f4b9674bd35eabbd612a
Jack Quick <Jack.Quick@mediware.com>
2019-11-18 12:43:19 -0600

    and yo

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -7,6 +7,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World in advance!");
+            Console.WriteLine("and yo");
         }
     }
 }
